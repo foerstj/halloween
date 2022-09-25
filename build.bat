5a84cd7d7532e165041b9d70283b248babbd389f
Johannes Förstner <johannes@famfoerstner.de>
2022-09-25 14:55:58 +0200

    Override skrubb effects

--- build.bat
+++ build.bat
@@ -18,6 +18,7 @@ robocopy "%doc_dsloa%\Bits\art" "%tmp%\Bits\art" /E /xf .gitignore /xf *.psd
 robocopy "%doc_dsloa%\Bits\sound" "%tmp%\Bits\sound" /E
 robocopy "%doc_dsloa%\Bits\world\contentdb" "%tmp%\Bits\world\contentdb" /E
 robocopy "%doc_dsloa%\Bits\world\global\moods" "%tmp%\Bits\world\global\moods" /E
+robocopy "%doc_dsloa%\Bits\world\global\effects" "%tmp%\Bits\world\global\effects" /E
 %tc%\RTC.exe -source "%tmp%\Bits" -out "%ds%\DSLOA\%mod_cs%.dsres" -copyright "CC-BY-SA 2022" -title "%mod_cs%" -author "Johannes Förstner"
 if %errorlevel% neq 0 pause
 
