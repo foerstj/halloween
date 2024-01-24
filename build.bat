f1f14c8ec1326a4e82459e1fd6c4e97874aaeabb
Johannes Förstner <johannes@famfoerstner.de>
2024-01-24 20:30:42 +0100

    Build translation file

--- build.bat
+++ build.bat
@@ -12,14 +12,23 @@ set ds=.
 :: path of TankCreator
 set tc=..\TankCreator
 
-:: Compile resource file
+set copyright=CC-BY-SA 2024
+set author=Johannes Förstner
+
+:: Compile main resource file
 rmdir /S /Q "%tmp%\Bits"
 robocopy "%doc_dsloa%\Bits\art" "%tmp%\Bits\art" /E /xf .gitignore /xf *.psd
 robocopy "%doc_dsloa%\Bits\sound" "%tmp%\Bits\sound" /E
-robocopy "%doc_dsloa%\Bits\world\contentdb" "%tmp%\Bits\world\contentdb" /E
+robocopy "%doc_dsloa%\Bits\world\contentdb\templates" "%tmp%\Bits\world\contentdb\templates" /E /xd git-ignore
 robocopy "%doc_dsloa%\Bits\world\global\moods" "%tmp%\Bits\world\global\moods" /E
 robocopy "%doc_dsloa%\Bits\world\global\effects" "%tmp%\Bits\world\global\effects" /E
-%tc%\RTC.exe -source "%tmp%\Bits" -out "%ds%\DSLOA\%mod_cs%.dsres" -copyright "CC-BY-SA 2022" -title "%mod_cs%" -author "Johannes Förstner"
+%tc%\RTC.exe -source "%tmp%\Bits" -out "%ds%\DSLOA\%mod_cs%.dsres" -copyright "%copyright%" -title "%mod_cs%" -author "%author%"
+if %errorlevel% neq 0 pause
+
+:: Compile German translation resource file
+rmdir /S /Q "%tmp%\Bits"
+robocopy "%doc_dsloa%\Bits\language" "%tmp%\Bits\language" *.de.gas /E
+%tc%\RTC.exe -source "%tmp%\Bits" -out "%ds%\DSLOA\%mod_cs%.de.dsres" -copyright "%copyright%" -title "%mod_cs%" -author "%author%"
 if %errorlevel% neq 0 pause
 
 :: Cleanup
