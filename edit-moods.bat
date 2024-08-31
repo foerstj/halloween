cc2e149d5bbf7a56d1e4cbe178acf3c8cf91bedc
Johannes Förstner <johannes@famfoerstner.de>
2024-08-31 10:02:22 +0200

    Use the --src param instead of copying them myself

--- edit-moods.bat
+++ edit-moods.bat
@@ -1,9 +1,7 @@
 :: path of Bits dir
 set bits=%~dp0.
 
-robocopy "%bits%\world\global\git-ignore\moods" "%bits%\world\global\moods" /S
-
 pushd %gaspy%
-venv\Scripts\python -m landscaping.edit_moods --edit-from-file mood-edits.txt --bits "%bits%"
+venv\Scripts\python -m landscaping.edit_moods --src "%bits%\world\global\git-ignore\moods" --edit-from-file mood-edits.txt --bits "%bits%"
 if %errorlevel% neq 0 pause
 popd
