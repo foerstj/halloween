a0cbdf3e1d7796292c901e286f6c51d1ceb419f0
Johannes Förstner <johannes@famfoerstner.de>
2025-10-14 12:08:04 +0200

    Add the demo map to version control, what gives

---
+++ run-demo.bat
@@ -0,0 +1,23 @@
+:: path of Bits dir
+set bits=%~dp0.
+:: path of DS installation
+set ds=%DungeonSiege%
+
+:: Compile resource file
+call "%bits%\build.bat" %*
+
+:: Compile map file
+set copyright=CC-BY-SA 2024
+set author=Johannes Förstner
+rmdir /S /Q "%tmp%\Bits"
+robocopy "%bits%\world\maps\halloween-demo" "%tmp%\Bits\world\maps\halloween-demo" /E
+"%tc%\RTC.exe" -source "%tmp%\Bits" -out "%ds%\DSLOA\halloween-demo.dsmap" -copyright "%copyright%" -title "%map_cs%" -author "%author%"
+if %errorlevel% neq 0 pause
+
+::pause
+
+:: Run it!
+"%ds%\DSLOA.exe" nointro=true map=halloween-demo
+
+:: Cleanup resources so as not to confuse Siege Editor
+call "%bits%\cleanup.bat" %*
