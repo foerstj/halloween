c44266b1624a76f71eec290b96aac70617ba7afe
Johannes Förstner <johannes@famfoerstner.de>
2026-09-19 16:40:36 +0200

    Adapt naming in run-demo.bat

--- run-demo.bat
+++ run-demo.bat
@@ -1,3 +1,8 @@
+:: name of mod
+set mod=halloween
+:: name of mod, case-sensitive
+set mod_cs=A Halloween Mod
+
 :: path of Bits dir
 set bits=%~dp0.
 :: path of DS installation
@@ -10,14 +15,14 @@ call "%bits%\build.bat" %*
 set copyright=CC-BY-SA 2025
 set author=Johannes Förstner
 rmdir /S /Q "%tmp%\Bits"
-robocopy "%bits%\world\maps\halloween-demo" "%tmp%\Bits\world\maps\halloween-demo" /E
-"%tc%\RTC.exe" -source "%tmp%\Bits" -out "%ds%\DSLOA\Halloween-demo.dsmap" -copyright "%copyright%" -title "%map_cs%" -author "%author%"
+robocopy "%bits%\world\maps\%mod%-demo" "%tmp%\Bits\world\maps\%mod%-demo" /E
+"%tc%\RTC.exe" -source "%tmp%\Bits" -out "%ds%\DSLOA\%mod_cs%-demo.dsmap" -copyright "%copyright%" -title "%map_cs%" -author "%author%"
 if %errorlevel% neq 0 pause
 
 ::pause
 
 :: Run it!
-"%ds%\DSLOA.exe" nointro=true map=halloween-demo
+"%ds%\DSLOA.exe" nointro=true map=%mod%-demo
 
 :: Cleanup resources so as not to confuse Siege Editor
 call "%bits%\cleanup.bat" %*
